918757e33fb08c930ebee595d31af72de798c3a9
gzachariadis <67559421+gzachariadis@users.noreply.github.com>
2023-12-30 14:03:13 +0200

    Adding reset of notes.txt

--- watch.py
+++ watch.py
@@ -10,6 +10,10 @@ from datetime import timedelta
 import datetime
 
 
+def is_non_zero_file(fpath):
+    return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
+
+
 class Handler(FileSystemEventHandler):
     def __init__(self, time=datetime.datetime.now()):
         self.time = time
@@ -18,10 +22,28 @@ class Handler(FileSystemEventHandler):
         time.sleep(5)
         os.system("cls")
         if event.src_path == ".\wars.py":
-            print("\n")
-            print("Output: ")
-            print("\n")
-            subprocess.run(["python", event.src_path])
+            if is_non_zero_file(
+                os.path.join(
+                    os.path.realpath(
+                        os.path.join(os.getcwd(), os.path.dirname(__file__))
+                    ),
+                    "wars.py",
+                )
+            ):
+                print("\n")
+                print("Output: ")
+                print("\n")
+                subprocess.run(["python", event.src_path])
+            else:
+                open(
+                    os.path.join(
+                        os.path.realpath(
+                            os.path.join(os.getcwd(), os.path.dirname(__file__))
+                        ),
+                        "notes.txt",
+                    ),
+                    "w",
+                ).close()
 
 
 if __name__ == "__main__":
@@ -32,7 +54,7 @@ if __name__ == "__main__":
 
     try:
         while True:
-            time.sleep(3)
+            time.sleep(5)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
